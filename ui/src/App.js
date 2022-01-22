ff437213ebd0c959b7f2dff9ccd95e9d98fab5e7
Michael Tighe <strideriidx@gmail.com>
2022-01-22 13:01:09 -0800

    Remove template files, add MUI

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import Box from '@mui/material/Box';
+
+const theme = createTheme({ palette: { mode: "dark" } });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Box sx={{ flexGrow: 1 }} m={2}>
+
+            </Box>
+        </ThemeProvider>
+    );
 }
 
 export default App;
